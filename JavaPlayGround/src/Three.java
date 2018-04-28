845134fcccb7328000b7b88a12ed826623c9b3a0
haominshi <haominshi@gmail.com>
2018-04-27 21:50:11 -0500

    update

---
+++ JavaPlayGround/src/Three.java
@@ -0,0 +1,5 @@
+public interface Three {
+
+    public int times(int a, int b, String name);
+
+}
--- JavaPlayGround/src/Two.java
+++ JavaPlayGround/src/Two.java
@@ -1,4 +1,4 @@
-public class Two extends One {
+public class Two extends One implements Three{
     int a = 1;
     int b = 2;
     String name;
@@ -17,10 +17,30 @@ public class Two extends One {
         return a+b;
     }
 
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int times(int a, int b, String name) {
+        System.out.println("The Obj Name is: " + name + "\n" + "And product is: ");
+        return a*b;
+    }
+
     public static void main(String [] args){
         Two two = new Two("Haha");
         two.doA();
         System.out.println(two.doBplusA(1,3));
+        System.out.print(two.times(two.getA(), two.getB(), two.getName()));
     }
+
 
 }
